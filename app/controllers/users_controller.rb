353d01a8c8621f01ca879c4f3aa3f01fdaa8c5db
TaroHiraishi <k3904283@kadai.jp>
2019-09-10 22:32:46 +0900

    Adjust routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to 'index'
+    if @user.save
+      redirect_to users_url
+    else
+      render 'new'
+    end
   end
 
   def show

a7937927fd106508d15494b7721d3290816a0c03
TaroHiraishi <k3904283@kadai.jp>
2019-09-10 16:50:35 +0900

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_072915) do
+ActiveRecord::Schema.define(version: 2019_09_10_073746) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

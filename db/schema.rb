c15696b2845dd9210225db9e9b57e7d98b4424ad
TaroHiraishi <k3904283@kadai.jp>
2019-09-11 10:38:23 +0900

    Add schedule model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_073746) do
+ActiveRecord::Schema.define(version: 2019_09_11_013342) do
+
+  create_table "schedules", force: :cascade do |t|
+    t.integer "year"
+    t.integer "month"
+    t.integer "week"
+    t.integer "day"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

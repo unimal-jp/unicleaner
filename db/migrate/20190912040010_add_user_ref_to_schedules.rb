d60ec5d81995494c896d6054537d3a84d447e6c5
TaroHiraishi <k3904283@kadai.jp>
2019-09-12 13:09:12 +0900

    user_idカラムを削除しました。

---
+++ db/migrate/20190912040010_add_user_ref_to_schedules.rb
@@ -0,0 +1,5 @@
+class AddUserRefToSchedules < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :schedules, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_032937) do
+ActiveRecord::Schema.define(version: 2019_09_12_040733) do
 
   create_table "schedules", force: :cascade do |t|
     t.integer "year"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_09_12_032937) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

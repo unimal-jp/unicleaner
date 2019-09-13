598aca80709daf9845871a7e5ba916290f448acf
TaroHiraishi <k3904283@kadai.jp>
2019-09-13 12:32:49 +0900

    usersテーブルを作りました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_015528) do
+ActiveRecord::Schema.define(version: 2019_09_13_033139) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.boolean "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end

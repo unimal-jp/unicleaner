9c2ffa580a27e10b661c1be2934852637405bb9e
TaroHiraishi <k3904283@kadai.jp>
2019-09-13 10:58:02 +0900

    scheduleテーブルを削除しました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_043601) do
-
-  create_table "schedules", force: :cascade do |t|
-    t.integer "year"
-    t.integer "month"
-    t.integer "week"
-    t.integer "day"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
+ActiveRecord::Schema.define(version: 2019_09_13_015528) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"

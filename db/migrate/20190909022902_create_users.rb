d7cc5985d96124641f68ea21f13f3e6bfa4699f0
TaroHiraishi <k3904283@kadai.jp>
2019-09-16 15:16:43 +0900

    Bootstrapを追加

--- db/migrate/20190909022902_create_users.rb
+++ db/migrate/20190909022902_create_users.rb
@@ -2,6 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :password
+      t.date :first_date_of_week
+      t.boolean :admin, default: false, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@ ActiveRecord::Schema.define(version: 2019_09_09_022902) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "password"
+    t.date "first_date_of_week"
+    t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

ab93720aa1a6c51f66679b57035b3a07aa8578fb
TaroHiraishi <k3904283@kadai.jp>
2019-09-16 17:54:53 +0900

    ログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :users
 
   root 'users#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end

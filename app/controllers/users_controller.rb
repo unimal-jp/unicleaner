df5da20943f3a6bca1033f254697dd2665d592ec
TaroHiraishi <k3904283@kadai.jp>
2019-09-16 16:01:33 +0900

    usersコントローラにeditアクション, updateアクションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_url
+    else
+      render :edit
+    end
+  end
+
   private
 
     def user_params

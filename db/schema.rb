ad03f2f285b40e04bb1e00dfb237c92cf3e0916b
TaroHiraishi <k3904283@kadai.jp>
2019-09-17 11:35:24 +0900

    scheduleモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_022902) do
+ActiveRecord::Schema.define(version: 2019_09_17_023356) do
+
+  create_table "schedules", force: :cascade do |t|
+    t.integer "user_id"
+    t.date "scheduled_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

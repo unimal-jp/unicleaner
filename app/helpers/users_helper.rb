c9d89b942851ff9b5220c90bae44825d0f3f8ebd
TaroHiraishi <k3904283@kadai.jp>
2019-09-17 15:17:46 +0900

    users/showでscheduled_dateがハイライトされます

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def dates(user)
+    week = []
+    sunday = user.first_date_of_week
+    7.times do
+      week.push(sunday)
+      sunday = sunday.next_day
+    end
+    week
+  end
+
 end

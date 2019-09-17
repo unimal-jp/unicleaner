0db016a6539fb7dba15f2691472e5d9e615aefb7
TaroHiraishi <k3904283@kadai.jp>
2019-09-17 14:19:44 +0900

    schedule/newにセレクトボックスを追加

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,2 +1,12 @@
 module SchedulesHelper
+
+  def option(user)
+    dates = [['Sun'], ['Mon'], ['Tue'], ['Wed'], ['Thu'], ['Fri'], ['Sat']]
+    sunday = user.first_date_of_week
+    dates.each do |date|
+      date.push(sunday)
+      sunday = sunday.next_day
+    end
+  end
+
 end

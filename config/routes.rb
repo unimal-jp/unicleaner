45d06498e4cb6780beb287961da09feb915db852
TaroHiraishi <k3904283@kadai.jp>
2019-09-19 10:39:35 +0900

    先月、来月の表示が完了しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :schedules
-  root 'schedules#index'
+  resources :schedules, only: [:new, :edit, :create, :update, :destroy]
+  get '/calendar/:year/:month', to: 'calendar#index', as: 'calendar'
+
+  root 'calendar#index'
 end
